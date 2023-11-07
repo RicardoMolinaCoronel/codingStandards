c3919adc26767c42bd93aacb23a65f537bb12d27
RicardoMolinaCoronel <ridumoli@espol.edu.ec>
2023-11-06 20:03:04 -0500

    Challenge

--- src/main/java/VacationPackageCostEstimation.java
+++ src/main/java/VacationPackageCostEstimation.java
@@ -12,13 +12,19 @@ public class VacationPackageCostEstimation {
     	Scanner scanner = new Scanner(System.in);
 
         // Get user inputs
-        System.out.println("Enter the destination of the vacation (Paris, New York City, or Other):");
+        System.out.println("Enter the destination of the vacation ( Paris, New York City, or Other):");
         String destination = scanner.nextLine();
         System.out.println("Enter the number of travelers:");
         int numTravelers = scanner.nextInt();
         System.out.println("Enter the duration of the vacation in days:");
         int duration = scanner.nextInt();
 
+        // Get optional add-ons
+        System.out.println("Select optional add-ons (separate with commas - '1' for All-Inclusive, '2' for Adventure Activities, '3' for Spa and Wellness):");
+        scanner.nextLine(); // Consume the newline character
+        String addOnsInput = scanner.nextLine();
+        String[] addOns = addOnsInput.split(",");
+        
         // Close the scanner
         scanner.close();
 
@@ -26,11 +32,11 @@ public class VacationPackageCostEstimation {
         double baseCost = 1000.0;
 
         // Check if the destination is a popular tourist spot and add the corresponding cost
-        if (destination.equalsIgnoreCase("Paris")) {
+        if ("Paris".equalsIgnoreCase(destination)) {
             baseCost += 500.0;
-        } else if (destination.equalsIgnoreCase("New York City")) {
+        } else if ("New York City".equalsIgnoreCase(destination)) {
             baseCost += 600.0;
-        } else if (!destination.equalsIgnoreCase("Other")) {
+        } else if (!"Other".equalsIgnoreCase(destination)) {
             // Invalid destination
             System.out.println("Invalid destination. Please choose from Paris, New York City, or Other.");
             System.out.println("Total cost: -1");
@@ -55,6 +61,18 @@ public class VacationPackageCostEstimation {
             totalCost += 200.0; // Penalty fee
         } else if (duration > 30 || numTravelers == 2) {
             totalCost -= 200.0; // Promotion
+        }
+        
+        // Calculate the cost of selected add-ons
+        for (String addOn : addOns) {
+            String addOnTrimmed = addOn.trim();
+            if ("1".equalsIgnoreCase(addOnTrimmed)) {
+                totalCost += 200.0 * numTravelers;
+            } else if ("2".equalsIgnoreCase(addOnTrimmed)) {
+                totalCost += 150.0 * numTravelers;
+            } else if ("3".equalsIgnoreCase(addOnTrimmed)) {
+                totalCost += 100.0 * numTravelers;
+            }
         }
 
         // Display the total cost
